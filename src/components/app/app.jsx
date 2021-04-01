eb37dfb1770c391c6e45b12d81f372a9ae558c30
Aleksey <eoew@yandex.ru>
2021-04-01 18:21:18 +0300

    Добавили заготовку Favorites

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Main from './main/main.jsx';
 import Room from './room/room.jsx';
 import SignIn from './sign-in/sign-in.jsx';
+import Favorites from './favorites/favorites.jsx';
 
 import TestComponent from './test-component/test-component-2.jsx';
 
@@ -18,6 +19,9 @@ const App = () => {
           <SignIn />
         </Route>
         <Route exact path="/offer:id" render={(serviceProps) => (<Room state={serviceProps.location.state} />)} />
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
         <Route exact path="/test">
           <TestComponent />
         </Route>
